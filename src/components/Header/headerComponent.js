bc572a5f83e815fec5f5a6d712039994b9991663
CleveyP <clevelandplonsey@gmail.com>
2023-01-04 16:00:31 -0800

    delete boilerplate in App.js create Logo component header component

---
+++ src/components/Header/headerComponent.js
@@ -0,0 +1,13 @@
+//header component 
+import React from 'react';
+import {Logo} from '../Logo/logoComponent';
+export let Header = () =>{
+    return(
+        <div className="Header">
+            
+            <Logo/>
+            
+        </div>        
+    );
+}
+
---
+++ src/components/Logo/logoComponent.js
@@ -0,0 +1,8 @@
+//logo component for the whole site
+import React from "react";
+import "./Logo.css";
+export let Logo = () =>{
+    return (
+        <img className="siteLogo"  src="../../../casinoLogo.png" alt="casino logo"/>
+    );
+};
