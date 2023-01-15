2f7eca60924f24eaf2b5da61470127817e7b4bd5
CleveyP <clevelandplonsey@gmail.com>
2023-01-14 16:33:05 -0800

    implement balance updating functions in slotMachineLogic

--- src/components/Games/SlotMachine/slotMachineLogic.js
+++ src/components/Games/SlotMachine/slotMachineLogic.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 
 //it costs 50 cents to spin the wheel
 const COST_TO_SPIN = 0.5;
@@ -158,43 +159,139 @@ export let playDispenseWinnings = () => {
 
 //update balance functions (send to backend and update balance after each spin)
 
-
 //pay function subtracts the cost of a spin from the player
 //if player has less than COST_TO_SPIN
 //returns the new balance of player after paying or -1 to indicate that player cannot afford to spin
+//or -666 to indicate error
 let payForSpin = async () =>{
     //getBalance
     let currentBalance = await getBalance();
+    if(currentBalance === -1){
+        console.log("could not get the user's current balance. The spin is prohibited");
+        return -666;
+    }
     //if player has less than COST_TO_SPIN
-    if(currentBalance < COST_TO_SPIN){
+    else if(currentBalance < COST_TO_SPIN){
+        console.log("Player cannot afford to spin.");
         return -1;
     }
     else{
         //pay for the spin
         currentBalance -= COST_TO_SPIN;
         //save the new balance in the database
-        await saveBalance(currentBalance);
+       let result = await saveBalance(currentBalance);
+       if(!result){
+            console.log("could not update the balance after trying to pay");
+            return -666;
+       }
         return currentBalance;
     }
 } 
 
-//validateSymbols returns how much player won from the spin
+//validateSymbols returns how much player won from the spin 
 let validateSymbols = () =>{
     //get the current symbols in the slot machine
     let symbol1, symbol2, symbol3;
+
+
     //if all symbols are the same then
     if(symbol1 === symbol2 && symbol3 === symbol2){
         //if all symbols are diamonds then
+
+        //if all symbols are orange then
+
+        //if all symbols are cherry then
+
+        //if all symbols are 7 then they won the jackpot so handle it
+
+        //if all symbols are BAR then the amount won is 0
     }
 }
-//getBalance function makes axios get request to grab current user balance from database
+
+//returns the user's current balance from database
+//returns -1 on failure to get balance
+let getBalance = async () =>{
+     //get the user email for the sql SELECT query
+     let email = document.getElementById("user-email").innerText;
+    if(email == undefined){
+        console.log("could not get user balance");
+        return -1;
+    }
+    //get request to grab user balance via SELECT by email
+    const response = await axios.get("user/balance", {userEmail: email});
+    if(response.success){
+        console.log("user balance is: "+ response.balance);
+        return parseFloat(response.balance);
+    }
+    else {
+        console.log("could not get user balance");
+        return -1;
+    }
+}
 
 //saveBalance function makes axios post request to put the new balance in the database
+//displays the new balance in the header
+//on success returns true on failure returns false
+let saveBalance = async (newBalance) =>{
+        
+        //get the user email for the sql SELECT query
+        let email = document.getElementById("user-email").innerText;
+        console.log("updating balance of: " + email);
+
+        if(email == undefined){
+            return false;
+        }
+        //post to backend the new balance 
+        const response = await axios.post("user/balance/update", {userEmail: email, userBalance: newBalance});
+        if(response.success){
+            console.log("succeeded in changing the user balance");
+            //TODO: update the header to the new balance
+            return true;
+        }
+        else{
+            console.log("error occurred! NO CHANGE TO BALANCE!!!");
+            return false;
+        }
+}
+
 
 //updateJackpot function makes axios post request to add .25 to the current jackpot if 
-//didWin is false if didWin is true then set jackpot to 300
+//didWin is false if didWin is true then set jackpot to 300 and return the old jackpot
 //then update the UI with the new value of the jackpot returned in the res object
-let updateJackpot = (didWin) =>{
-    
+let updateJackpot = async (didWin) =>{
+    let currentJackpot;
+    let jackpotElement = document.getElementById("slot-machine-jackpot");
+    console.log("current jackpot: " + newJackpot);
+    //get current jackpot
+    const response1 = await axios.get("/jackpot");
+    if(response1.success){
+        currentJackpot = parseFloat(response1.jackpot); 
+        let oldJackpot = currentJackpot;
+         //if the user won the jackpot, reset it to 300 otherwise add half the user's bet to the jackpot
+        currentJackpot = (didWin) ? 300 : ( COST_TO_SPIN / 3 ) + currentJackpot;
+        //request that the database should be updated
+        const response = await axios.post("/jackpot", {newJackpot: currentJackpot});
+        if(response.success){
+            //update the UI so that it shows the new jackpot
+            jackpotElement.innerText = currentJackpot;
+            
+            //return the payout if the user won the jackpot
+            if(didWin){
+                return oldJackpot;
+            }
+        
+            else{
+                return 0;
+            } 
+        }
+
+        else{
+            console.log("Error in updating jackpot");
+        }
+    }
+    else{
+        console.log("could not get current jackpot from DB");
+        return -1;
+    } 
 }
 
